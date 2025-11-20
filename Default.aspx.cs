422a505a5cec2a1f6b64b4143f0ccdc4975b60da
MohsinRazuan <138411977+MohsinRazuan@users.noreply.github.com>
2025-11-20 09:07:05 +0600

    Initial commit

---
+++ Default.aspx.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+//using TDBWebService;
+
+public partial class _Default : System.Web.UI.Page
+{
+    protected void Page_Load(object sender, EventArgs e)
+    {
+        try
+        {
+            //TDBWebService.WebServiceSoapClient tdbS = new TDBWebService.WebServiceSoapClient();
+
+            //lblWS.Text=tdbS.eContractorSignUp("mr_contractor8", 2, "mr_contractor8@dbgurus.com.au", "dbg123!", "", "");
+           
+        }
+        catch(Exception ex)
+        {
+            lblWS.Text=ex.Message + " ---"+ ex.StackTrace;
+        }
+        
+
+    }
+}
